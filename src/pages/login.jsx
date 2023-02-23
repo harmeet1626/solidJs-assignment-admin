b89e4753dc3529a572a5c3d2b63f621039157aff
Harmeet Singh <harmeet_singh1@softprodigy.com>
2023-02-23 13:20:18 +0530

    made password dynamic

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -31,8 +31,8 @@ const login = () => {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          username: "kminchelle",
-          password: "0lelplR",
+          username: username(),
+          password: password(),
         }),
       })
         .then((res) => res.json())
