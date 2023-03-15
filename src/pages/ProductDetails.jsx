f6c3e950852cf4112537964f433c55bac38b9d2a
Harmeet Singh <harmeet_singh1@softprodigy.com>
2023-03-15 11:27:46 +0530

    loader added in userdetails page and product details page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -4,12 +4,14 @@ import { createStore, produce } from "solid-js/store";
 import toast, { Toaster } from "solid-toast";
 import "../style/input.css";
 const productDetails = () => {
+  const [isLoading, setisLoading] = createSignal(false);
   const Params = useParams();
   function getId() {
     return { ...Params }.id;
   }
   const navigate = useNavigate();
   const getProducts = async () => {
+    setisLoading(true);
     return await (await fetch(`https://dummyjson.com/products/${getId()}`))
       .json()
       .then((res) => {
@@ -26,6 +28,7 @@ const productDetails = () => {
               (data.stock = res.stock);
           })
         );
+        setisLoading(false);
         return res;
       });
   };
@@ -45,10 +48,12 @@ const productDetails = () => {
 
   function DeleteProduct() {
     try {
+      setisLoading(true);
       fetch(`https://dummyjson.com/products/${getId()}`, {
         method: "DELETE",
       })
         .then((res) => res.json())
+        .then(setisLoading(false))
         .then(toast.success("Product Deleted"))
         .then(navigate("/allProducts"));
     } catch (e) {
@@ -81,139 +86,149 @@ const productDetails = () => {
   }
   return (
     <div>
-      <div style={"padding-left:40px; padding-top:30px"}>
-        <form onsubmit={submit}>
-          <div class="container">
-            <br></br>
-            <h1>Update Product</h1>
-            <div align="right">
-              <button
-                onClick={() => DeleteProduct()}
-                type="button"
-                class="btn btn-danger"
+      {isLoading() == true ? (
+        <div class="loader"></div>
+      ) : (
+        <div style={"padding-left:40px; padding-top:30px"}>
+          <form onsubmit={submit}>
+            <div class="container">
+              <br></br>
+              <h1>Update Product</h1>
+              <div align="right">
+                <button
+                  onClick={() => DeleteProduct()}
+                  type="button"
+                  class="btn btn-danger"
+                >
+                  Delete Product
+                </button>
+              </div>
+              <div style="display flex">
+                <p>Please fill in this form to create an account.</p>
+              </div>
+              <hr></hr>
+
+              <label for="text">
+                <b>Title</b>
+              </label>
+              <input
+                class="my-input"
+                style={"width:400px; margin-left: 272px;"}
+                value={Product()?.title}
+                onInput={(e) =>
+                  setproductDetails("title", e.currentTarget.value)
+                }
+                type="text"
+                placeholder="Enter Title"
+                required
+              />
+              <br></br>
+
+              <label for="price">
+                <b>Category</b>
+              </label>
+              <input
+                class="my-input"
+                style={"width:400px; margin-left: 237px;"}
+                value={Product()?.category}
+                onInput={(e) =>
+                  setproductDetails("category", e.currentTarget.value)
+                }
+                type="text"
+                placeholder="Enter Category"
+                required
+              />
+              <br></br>
+              <label for="price">
+                <b>Price</b>
+              </label>
+              <input
+                class="my-input"
+                style={"width:400px; margin-left: 269px;"}
+                value={Product()?.price}
+                onInput={(e) => setproductDetails("age", e.currentTarget.value)}
+                type="number"
+                placeholder="Enter Price"
+                required
+              />
+              <br></br>
+              <label for="price">
+                <b>Brand</b>
+              </label>
+              <input
+                class="my-input"
+                style={"width:400px; margin-left: 262px;"}
+                value={Product()?.brand}
+                onInput={(e) =>
+                  setproductDetails("brand", e.currentTarget.value)
+                }
+                type="text"
+                placeholder="Enter Brand"
+                required
+              />
+              <br></br>
+
+              <label for="email">
+                <b>DiscountPercentage</b>
+              </label>
+              <input
+                class="my-input"
+                style={"width:400px; margin-left: 153px;"}
+                value={`${Product()?.discountPercentage}%`}
+                onInput={(e) =>
+                  setproductDetails("discountPercentage", e.currentTarget.value)
+                }
+                type="text"
+                placeholder="Enter Discount Percentage"
+                required
+              />
+              <br></br>
+              <label>
+                <b>Stock</b>
+              </label>
+              <input
+                class="my-input"
+                style={"width:400px; margin-left: 266px;"}
+                value={`${Product()?.stock}`}
+                onInput={(e) =>
+                  setproductDetails("stock", e.currentTarget.value)
+                }
+                type="number"
+                placeholder="Enter stock"
+                required
+              />
+              <br></br>
+              <div style={"display:flex"}></div>
+              <label for="text">
+                <b>Description</b>
+              </label>
+              <br></br>
+              <textarea
+                style={" margin-left: 303px;"}
+                value={Product()?.description}
+                onInput={(e) =>
+                  setproductDetails("description", e.currentTarget.value)
+                }
+                rows="4"
+                cols="50"
+                name="comment"
+                form="usrform"
               >
-                Delete Product
+                {Product()?.description}
+              </textarea>
+              <br></br>
+              <hr />
+              <p>
+                By creating an account you agree to our <a>Terms & Privacy</a>.
+              </p>
+
+              <button style={"width: 725px;"} type="submit" class="registerbtn">
+                Update
               </button>
             </div>
-            <div style="display flex">
-              <p>Please fill in this form to create an account.</p>
-            </div>
-            <hr></hr>
-
-            <label for="text">
-              <b>Title</b>
-            </label>
-            <input
-              class="my-input"
-              style={"width:400px; margin-left: 272px;"}
-              value={Product()?.title}
-              onInput={(e) => setproductDetails("title", e.currentTarget.value)}
-              type="text"
-              placeholder="Enter Title"
-              required
-            />
-            <br></br>
-
-            <label for="price">
-              <b>Category</b>
-            </label>
-            <input
-              class="my-input"
-              style={"width:400px; margin-left: 237px;"}
-              value={Product()?.category}
-              onInput={(e) =>
-                setproductDetails("category", e.currentTarget.value)
-              }
-              type="text"
-              placeholder="Enter Category"
-              required
-            />
-            <br></br>
-            <label for="price">
-              <b>Price</b>
-            </label>
-            <input
-              class="my-input"
-              style={"width:400px; margin-left: 269px;"}
-              value={Product()?.price}
-              onInput={(e) => setproductDetails("age", e.currentTarget.value)}
-              type="number"
-              placeholder="Enter Price"
-              required
-            />
-            <br></br>
-            <label for="price">
-              <b>Brand</b>
-            </label>
-            <input
-              class="my-input"
-              style={"width:400px; margin-left: 262px;"}
-              value={Product()?.brand}
-              onInput={(e) => setproductDetails("brand", e.currentTarget.value)}
-              type="text"
-              placeholder="Enter Brand"
-              required
-            />
-            <br></br>
-
-            <label for="email">
-              <b>DiscountPercentage</b>
-            </label>
-            <input
-              class="my-input"
-              style={"width:400px; margin-left: 153px;"}
-              value={`${Product()?.discountPercentage}%`}
-              onInput={(e) =>
-                setproductDetails("discountPercentage", e.currentTarget.value)
-              }
-              type="text"
-              placeholder="Enter Discount Percentage"
-              required
-            />
-            <br></br>
-            <label>
-              <b>Stock</b>
-            </label>
-            <input
-              class="my-input"
-              style={"width:400px; margin-left: 266px;"}
-              value={`${Product()?.stock}`}
-              onInput={(e) => setproductDetails("stock", e.currentTarget.value)}
-              type="number"
-              placeholder="Enter stock"
-              required
-            />
-            <br></br>
-            <div style={"display:flex"}></div>
-            <label for="text">
-              <b>Description</b>
-            </label>
-            <br></br>
-            <textarea
-              style={" margin-left: 303px;"}
-              value={Product()?.description}
-              onInput={(e) =>
-                setproductDetails("description", e.currentTarget.value)
-              }
-              rows="4"
-              cols="50"
-              name="comment"
-              form="usrform"
-            >
-              {Product()?.description}
-            </textarea>
-            <br></br>
-            <hr />
-            <p>
-              By creating an account you agree to our <a>Terms & Privacy</a>.
-            </p>
-
-            <button style={"width: 725px;"} type="submit" class="registerbtn">
-              Update
-            </button>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/userDetails.jsx
+++ src/pages/userDetails.jsx
@@ -1,27 +1,37 @@
-import { createResource } from "solid-js";
+import { createResource, createSignal } from "solid-js";
 import { useParams, useNavigate } from "@solidjs/router";
 import { createStore } from "solid-js/store";
 import toast from "solid-toast";
 import "../style/input.css";
 
 const userDetails = () => {
+  const [isLoading, setisLoading] = createSignal(false);
   const navigate = useNavigate();
   const Params = useParams();
   function getId() {
     return { ...Params }.id;
   }
   function deleteUser() {
+    setisLoading(true);
     fetch(`https://dummyjson.com/users/${getId()}`, {
       method: "DELETE",
     })
       .then((res) => res.json())
       .then((res) => {
+        setisLoading(false);
         toast.success("user deleted");
         navigate("/Allusers");
       });
   }
-  const fetchUser = async () =>
-    await (await fetch(`https://dummyjson.com/users/${getId()}`)).json();
+  const fetchUser = async () => {
+    setisLoading(true);
+    return await (await fetch(`https://dummyjson.com/users/${getId()}`))
+      .json()
+      .then((res) => {
+        setisLoading(false);
+        return res;
+      });
+  };
   const [users] = createResource(fetchUser);
 
   const [userDetails, setuserdetails] = createStore({
@@ -52,95 +62,101 @@ const userDetails = () => {
   }
   return (
     <>
-      <div style={"padding-left:40px; padding-top:30px"}>
-        <form onSubmit={edit}>
-          <div class="container">
-            <h1>Update user</h1>
-            <div style="display flex">
-              <p>Please fill in this form to create an account.</p>
-              <div align="right">
-                <button
-                  onClick={() => deleteUser()}
-                  type="button"
-                  class="btn btn-danger"
-                >
-                  Delete user
-                </button>
+      {isLoading() == true ? (
+        <div class="loader"></div>
+      ) : (
+        <div style={"padding-left:40px; padding-top:30px"}>
+          <form onSubmit={edit}>
+            <div class="container">
+              <h1>Update user</h1>
+              <div style="display flex">
+                <p>Please fill in this form to create an account.</p>
+                <div align="right">
+                  <button
+                    onClick={() => deleteUser()}
+                    type="button"
+                    class="btn btn-danger"
+                  >
+                    Delete user
+                  </button>
+                </div>
               </div>
+              <hr></hr>
+
+              <label for="text">
+                <b>Firstname</b>
+              </label>
+              <input
+                class="my-input"
+                style={"width:400px; margin-left: 272px;"}
+                value={users()?.firstName}
+                onInput={(e) =>
+                  setuserdetails("firstName", e.currentTarget.value)
+                }
+                type="text"
+                placeholder="Enter firstname"
+                required
+              />
+              <br></br>
+
+              <label for="text">
+                <b>Lastname</b>
+              </label>
+              <input
+                class="my-input"
+                style={"width:400px; margin-left: 274px;"}
+                value={users()?.lastName}
+                onInput={(e) =>
+                  setuserdetails("lastName", e.currentTarget.value)
+                }
+                type="text"
+                placeholder="Enter lastname"
+                required
+              />
+              <br></br>
+
+              <label for="email">
+                <b>Age</b>
+              </label>
+              <input
+                class="my-input"
+                style={"width:400px; margin-left: 319px;"}
+                value={users()?.age}
+                onInput={(e) => setuserdetails("age", e.currentTarget.value)}
+                type="number"
+                placeholder="Enter age"
+                required
+              />
+              <br></br>
+
+              <label for="email">
+                <b>Email</b>
+              </label>
+              <input
+                class="my-input"
+                style={"width:400px; margin-left: 305px;"}
+                value={users()?.email}
+                onInput={(e) => setuserdetails("email", e.currentTarget.value)}
+                type="email"
+                placeholder="Enter Email"
+                name="email"
+                id="email"
+                required
+              />
+              <br></br>
+
+              <hr />
+              <p>
+                By creating an account you agree to our <a>Terms & Privacy</a>.
+              </p>
+
+              <button style={"width: 530px;"} type="submit" class="registerbtn">
+                Update
+              </button>
             </div>
-            <hr></hr>
-
-            <label for="text">
-              <b>Firstname</b>
-            </label>
-            <input
-              class="my-input"
-              style={"width:400px; margin-left: 272px;"}
-              value={users()?.firstName}
-              onInput={(e) =>
-                setuserdetails("firstName", e.currentTarget.value)
-              }
-              type="text"
-              placeholder="Enter firstname"
-              required
-            />
-            <br></br>
-
-            <label for="text">
-              <b>Lastname</b>
-            </label>
-            <input
-              class="my-input"
-              style={"width:400px; margin-left: 274px;"}
-              value={users()?.lastName}
-              onInput={(e) => setuserdetails("lastName", e.currentTarget.value)}
-              type="text"
-              placeholder="Enter lastname"
-              required
-            />
-            <br></br>
-
-            <label for="email">
-              <b>Age</b>
-            </label>
-            <input
-              class="my-input"
-              style={"width:400px; margin-left: 319px;"}
-              value={users()?.age}
-              onInput={(e) => setuserdetails("age", e.currentTarget.value)}
-              type="number"
-              placeholder="Enter age"
-              required
-            />
-            <br></br>
-
-            <label for="email">
-              <b>Email</b>
-            </label>
-            <input
-              class="my-input"
-              style={"width:400px; margin-left: 305px;"}
-              value={users()?.email}
-              onInput={(e) => setuserdetails("email", e.currentTarget.value)}
-              type="email"
-              placeholder="Enter Email"
-              name="email"
-              id="email"
-              required
-            />
-            <br></br>
-
-            <hr />
-            <p>
-              By creating an account you agree to our <a>Terms & Privacy</a>.
-            </p>
-
-            <button style={"width: 530px;"} type="submit" class="registerbtn">
-              Update
-            </button>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      )}
     </>
   );
 };
